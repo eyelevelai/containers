4cf5850d304e42cc21184d82e036242bbbc07270
eyelevelai <33876565+eyelevelai@users.noreply.github.com>
2024-09-02 09:33:40 -0600

    On Prem Containers

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # containers
+
+Builds containers and pushes them to EyeLevel.ai public ECR public repositories.
+
+```
+./build.sh {FOLDER_NAME} (optional for build only) -n
+```
+
+If -n or --no-push is added to the build command, the Docker container will be built but not pushed.
+
+Run the following command to debug the containers locally:
+
+```
+docker run --platform linux/amd64 -it public.ecr.aws/c9r4x6y5/eyelevel/{FOLDER_NAME}:latest /bin/bash
+```
---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Default flag value for pushing
+push_flag=true
+app=""
+
+# Loop through all arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --no-push|-n)
+      push_flag=false
+      shift # Remove the flag from the argument list
+      ;;
+    *)
+      app=$1
+      shift # Remove the app name from the argument list
+      ;;
+  esac
+done
+
+# Check if the app variable is set
+if [ -z "$app" ]; then
+  echo "Error: No application name provided."
+  echo "Usage: $0 [--no-push|-n] <app_name>"
+  exit 1
+fi
+
+# Check if the directory exists
+if [ ! -d "$app" ]; then
+  echo "Error: Directory '$app' does not exist."
+  exit 1
+fi
+
+docker build --platform linux/amd64 -t public.ecr.aws/c9r4x6y5/eyelevel/$app:latest $app
+
+# Log in to AWS ECR and push if push_flag is true
+if [ "$push_flag" = true ]; then
+  aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+  docker push public.ecr.aws/c9r4x6y5/eyelevel/$app:latest
+fi
